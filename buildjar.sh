d23a284bcf3ac796835096fc722fdb9a068dcc0e
Michael Henke <smack42@googlemail.com>
2015-03-14 23:29:16 +0100

    use advzip (zopfli) to build a smaller jar file

--- buildjar.sh
+++ buildjar.sh
@@ -1,7 +1,17 @@
 OUTPUT=colorfill.jar
+TMPDIR=_tmp_
 
-rm -f $OUTPUT
+rm -f $OUTPUT _$OUTPUT
 
-java -jar $HOME/Programme/proguard5.1/lib/proguard.jar @proguard_config -outjars $OUTPUT
+java -jar $HOME/Programme/proguard5.1/lib/proguard.jar @proguard_config -outjars _$OUTPUT
+
+rm -rf $TMPDIR
+mkdir $TMPDIR
+unzip -q _$OUTPUT -d $TMPDIR
+
+cd $TMPDIR
+advzip -a -4 -i 256 ../$OUTPUT *
+cd ..
+rm -rf $TMPDIR
 
 chmod u+x $OUTPUT
