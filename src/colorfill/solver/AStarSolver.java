7d75c2cf98e53550bba5d67eb76e43d74824f3af
Michael Henke <smack42@gmail.com>
2020-10-24 23:21:36 +0200

    use XXHash32 in the custom HashMap used in AStarSolver
    currently deactivated, because first tests did not show a performance improvement over MurmurHash3

--- src/colorfill/solver/AStarSolver.java
+++ src/colorfill/solver/AStarSolver.java
@@ -519,6 +519,58 @@ next:   for (int nextColorNeighbor;  (nextColorNeighbor = nextColorNeighbors.nex
             h1 ^= h1 >>> 16;
             return h1;
         }
+//        // based on XXHash32
+//        // https://github.com/lz4/lz4-java
+//        // https://github.com/richardstartin/xxhash-benchmark
+//        private static final int PRIME1 = 0x9E3779B1;
+//        private static final int PRIME2 = 0x85EBCA77;
+//        private static final int PRIME3 = 0xC2B2AE3D;
+//        private static final int PRIME4 = 0x27D4EB2F;
+//        private static final int PRIME5 = 0x165667B1;
+//        private static final int seed = 12345;
+//        private int hash(final long[] key, final int startIndex) {
+//            int h32;
+//            int k = startIndex;
+//            final int km = startIndex + this.KEY_SIZE;
+//            if (this.KEY_SIZE >= 2) { // operate on 4 streams of 32 bits in parallel
+//                int v1 = seed + PRIME1 + PRIME2;
+//                int v2 = seed + PRIME2;
+//                int v3 = seed;
+//                int v4 = seed - PRIME1;
+//                do {
+//                    final long l1 = key[k++];
+//                    v1 += (int)(l1) * PRIME2;
+//                    v1 = Integer.rotateLeft(v1, 13);
+//                    v1 *= PRIME1;
+//                    v2 += (int)(l1 >>> 32) * PRIME2;
+//                    v2 = Integer.rotateLeft(v2, 13);
+//                    v2 *= PRIME1;
+//                    final long l2 = key[k++];
+//                    v3 += (int)(l2) * PRIME2;
+//                    v3 = Integer.rotateLeft(v3, 13);
+//                    v3 *= PRIME1;
+//                    v4 += (int)(l2 >>> 32) * PRIME2;
+//                    v4 = Integer.rotateLeft(v4, 13);
+//                    v4 *= PRIME1;
+//                } while (k < km - 1);
+//                h32 = Integer.rotateLeft(v1, 1) + Integer.rotateLeft(v2, 7) + Integer.rotateLeft(v3, 12) + Integer.rotateLeft(v4, 18);
+//            } else {
+//                h32 = seed + PRIME5;
+//            }
+//            while (k < km) {
+//                final long l = key[k++];
+//                h32 += (int)(l) * PRIME3;
+//                h32 = Integer.rotateLeft(h32, 17) * PRIME4;
+//                h32 += (int)(l >>> 32) * PRIME3;
+//                h32 = Integer.rotateLeft(h32, 17) * PRIME4;
+//            }
+//            h32 ^= h32 >>> 15;
+//            h32 *= PRIME2;
+//            h32 ^= h32 >>> 13;
+//            h32 *= PRIME3;
+//            h32 ^= h32 >>> 16;
+//            return h32;
+//        }
 
         /**
          * double the storage space in the internal tables
