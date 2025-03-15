e6b2fac172bb9f901bc21f9a96b95db0f46c0023
Michael Henke <smack42@gmail.com>
2025-03-15 18:57:42 +0100

    update FlatLaf Look&Feel to 3.5.4

--- buildjar.sh
+++ buildjar.sh
@@ -6,14 +6,14 @@ if [ ! -f $TMPLIBZIP ]; then
     rm -rf $TMPDIR
     mkdir $TMPDIR
     unzip -q lib/designgridlayout-1.11.jar -x "META-INF/*" -d $TMPDIR
-    unzip -q lib/flatlaf-3.2.jar           -x "META-INF/*" -d $TMPDIR
+    unzip -q lib/flatlaf-3.5.4.jar         -x "META-INF/*" -d $TMPDIR
     cd $TMPDIR
     advzip -a -4 -i 1000 ../$TMPLIBZIP *
     cd ..
 fi
 
 rm -f $OUTPUT _$OUTPUT $OUTPUT.zip
-java -jar $HOME/Programme/proguard6.2/lib/proguard.jar @proguard_config -outjars _$OUTPUT
+java -jar $HOME/Programme/proguard7.6/lib/proguard.jar @proguard_config -outjars _$OUTPUT
 
 rm -rf $TMPDIR
 mkdir $TMPDIR
